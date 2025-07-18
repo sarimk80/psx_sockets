a2bbc297678799223696cf2a5a549342311624f8
Sarim <sarimk80@gmail.com>
2025-07-18 11:49:30 +0500

    Initial Commit

---
+++ psx_sockets/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  psx_sockets
+//
+//  Created by sarim khan on 18/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ psx_sockets/psx_socketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  psx_socketsApp.swift
+//  psx_sockets
+//
+//  Created by sarim khan on 18/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct psx_socketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
